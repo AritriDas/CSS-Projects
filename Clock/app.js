32352a52bd2af993c24a059fd495faf625119dab
AritriDas <dasaritri07@gmail.com>
2020-05-18 21:55:23 +0530

    Changes made

--- Clock/app.js
+++ Clock/app.js
@@ -1,3 +1,18 @@
 const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
+
+function showTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let sec = date.getSeconds();
+
+hours.innerHTML = `${hour}`;
+minutes.innerHTML=`${minute}`;
+seconds.innerHTML= `${sec}`;
+
+}
+
+showTime();
+setInterval(showTime,1000);
